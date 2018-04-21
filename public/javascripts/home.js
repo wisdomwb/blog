6272bc4c15fd5ff6e1141df03e4a0b83ec358e34
weizhen <a610428@qq.com>
2018-04-21 11:14:24 +0800

    update: 匹配静态文件

---
+++ public/javascripts/home.js
@@ -0,0 +1,50 @@
+
+
+//发送请求
+function sendRequest(url, method) {
+    const promise = new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest()
+      // 指定通信过程中状态改变时的回调函数
+      const handler = function () {
+        // 4表示请求已完成并且响应已准备好
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(this.response)
+            // document.querySelector('#resultBox').innerHTML = xhr.responseText
+          } else {
+            reject(new Error(this.statusText))
+            // document.querySelector('#resultBox').innerHTML = xhr.responseText
+          }
+        }
+      }
+      xhr.onreadystatechange = handler
+      xhr.onerror = function (e) {
+        document.write(e)
+      }
+  
+      // open方式用于指定HTTP动词、请求的网址、是否异步
+      xhr.open(method, url, true)
+  
+      //请求头
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+      let myVar = 'weizhen'
+      // 发送HTTP请求
+      xhr.send("name=value&anothername=" + encodeURIComponent(myVar) + "&so=on")
+    })
+    return promise
+  }
+  
+  //绑定事件
+  let el = document.querySelector('#home')
+  el.addEventListener('click', () => {
+    //先清空结果
+    // document.querySelector('#resultBox').innerHTML = ''
+  
+    //发送请求
+    let url = 'http://127.0.0.1:3000/home', method = 'GET'
+    sendRequest(url, method).then(json => {
+      // document.querySelector('#resultBox').innerHTML = json
+    }, error => {
+      // document.querySelector('#resultBox').innerHTML = '出错了' + error
+    })
+  })
--- server.js
+++ server.js
@@ -12,21 +12,38 @@ let result = {
 
 exports.start = () => {
   http.createServer((req, res) => {
-    fs.readFile('./views/home.html', 'utf-8', (err, data) => {
-      if (err) {
-        throw err
-      }
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'text/html')
-      res.setHeader('Access-Control-Allow-Origin', '*')
-      let post = ''
-      req.on('data', (chunk) => {
-        post += chunk
+    let pathname = url.parse(req.url).pathname
+    console.log(pathname, 'pathname')
+    //请求public目录
+    let regex0 = /\/javascripts|\/stylesheets|\/images/
+    //获取后缀
+    let ext = /(\.[^.]+|)$/.exec(pathname)[0]
+    if (ext === '.css' || ext === '.js') {
+      fs.readFile(`.publick/${pathname}`, 'utf-8', (err, data) => {
+        if (err) {
+          throw err
+        }
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        res.setHeader('Content-Type', 'text/html')
       })
-      req.on('end', () => {
-        res.end(data)
+    } else {
+      fs.readFile('./views/home.html', 'utf-8', (err, data) => {
+        if (err) {
+          throw err
+        }
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        // res.setHeader('Access-Control-Allow-Origin', '*')
+        let post = ''
+        req.on('data', (chunk) => {
+          post += chunk
+        })
+        req.on('end', () => {
+          res.end(data)
+        })
       })
-    })
+    }
 
   }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`)
